e955bc2d45a6e77e85a519882b2b0be95b40a075
dipti95 <dipti.yadav@outlook.com>
2022-11-11 23:59:49 +0000

    updated code

--- main.go
+++ main.go
@@ -10,6 +10,10 @@ import (
 	"github.com/gorilla/mux"
 )
 
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to CRM Backend Project")
+}
+
 func getAllCustomer(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
@@ -43,9 +47,11 @@ func createCustomer(w http.ResponseWriter, r *http.Request) {
 
 	json.Unmarshal(reqBody, &newCustomer)
 
+	//Customers in the database have unique ID values
 	newCustomer.Id = uuid.New().String()
 
 	customers = append(customers, newCustomer)
+	w.WriteHeader(http.StatusCreated)
 
 	json.NewEncoder(w).Encode(customers)
 
@@ -83,7 +89,7 @@ func updateCustomer(w http.ResponseWriter, r *http.Request) {
 		if customer.Id == Id {
 			c := &customers[idx]
 
-			c.Id = newCustomer.Id
+			c.Id = customer.Id
 
 			if len(newCustomer.Name) != 0 {
 				c.Name = newCustomer.Name
@@ -120,14 +126,22 @@ func updateCustomer(w http.ResponseWriter, r *http.Request) {
 func main() {
 
 	router := mux.NewRouter()
+
+	router.HandleFunc("/", index).Methods("GET")
+
+	// Getting a list of all customers
 	router.HandleFunc("/customers", getAllCustomer).Methods("GET")
 
+	//Getting data for a single customer
 	router.HandleFunc("/customers/{id}", getCustomer).Methods("GET")
 
+	//Adding a customer
 	router.HandleFunc("/customers", createCustomer).Methods("POST")
 
+	//Removing a customer
 	router.HandleFunc("/customers/{id}", deleteCustomer).Methods("DELETE")
 
+	//Updating a customer's information
 	router.HandleFunc("/customers/{id}", updateCustomer).Methods("PATCH")
 
 	fmt.Println("Server is starting...")
